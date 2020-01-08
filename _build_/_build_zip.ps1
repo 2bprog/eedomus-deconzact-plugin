b7a64dbccc47134cf504c7cb78ee214ba19008f7
Benjamin B <git@bourderon.fr>
2020-01-08 16:45:57 +0100

    Update _build_zip.ps1

--- _build_/_build_zip.ps1
+++ _build_/_build_zip.ps1
@@ -1,3 +1,8 @@
+# script eedomus-deconzact-plugin
+
+$zip = ".\release\deconzact.zip"  
+
+
 $scriptpath = $MyInvocation.MyCommand.Path
 $dir = Split-Path $scriptpath
 set-location $dir
@@ -9,49 +14,22 @@ if (-Not (Test-Path ".\tmp\img"))     {  New-Item -Path ".\tmp" -Name "img" -Ite
 Remove-Item ".\tmp\*.*"
 Remove-Item ".\tmp\img\*.*"
 
-$zip = ".\release\deconzact.zip"  
 if (Test-Path $zip) {  Remove-Item $zip }
-
-
-# Copy-Item -Path "..\img\deconzact.*" -Destination ".\tmp\img\" -Force
 
 Copy-Item -Path "..\img\*.png" -Destination ".\tmp\img\" -Force
 Copy-Item -Path "..\php\*.php" -Destination ".\tmp" -Force
 Copy-Item "..\eedomus_plugin.json" -Destination ".\tmp" -Force
 Copy-Item "..\readme_fr.md" -Destination ".\tmp" -Force
 
-# Template : (Get-Content $json) | Foreach-Object {$_ -replace 'XX', 'YY'} | Set-Content $file.$json
 $json = ".\tmp\eedomus_plugin.json"
-# (Get-Content $json) | Foreach-Object {$_ -replace '"plugin_id": "bbdeconzact"'    , '"plugin_id": "bbdeconzacti"'} | Set-Content $json
-# (Get-Content $json) | Foreach-Object {$_ -replace '"name_fr": "Actionneurs - deConz"', '"name_fr": "Actionneurs - deConz [Internal]"'} | Set-Content $json
-,
-(Get-Content $json) | Foreach-Object {$_ -replace '"prise.png","icon2b":'     , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"prise_b2.png","icon2b":'  , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"prise_b1.png","icon2b":'  , ''} | Set-Content $json
 
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_on.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_off.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_mid.png","icon2b":'   , ''} | Set-Content $json
+# Garder icones eedomus 
+# ---------------------
+# (Get-Content $json) | Foreach-Object {$_ -replace '"{1}icon2b"{1}.{0,}"{1}.{1,}"{1}.{0,},'   , ''} | Set-Content $json
 
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_cold.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_warm.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_02.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_03.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_04.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_05.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_06.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_07.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_08.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_38.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_43.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"lamp_46.png","icon2b":'   , ''} | Set-Content $json
-
-(Get-Content $json) | Foreach-Object {$_ -replace '"heartbeat.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"heartbeat_c2.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"heartbeat_c1.png","icon2b":'   , ''} | Set-Content $json
-
-(Get-Content $json) | Foreach-Object {$_ -replace '"wait.png","icon2b":'   , ''} | Set-Content $json
-(Get-Content $json) | Foreach-Object {$_ -replace '"simulation.png","icon2b":'   , ''} | Set-Content $json
+# Garder mes icones    
+# ---------------------
+(Get-Content $json) | Foreach-Object {$_ -replace ':{1}.{0,}"{1}.{1,}"icon2b"'  , ''} | Set-Content $json
 
 
 $compress = @{
